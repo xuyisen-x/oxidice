96b2d9a52f96d1180ee535d66cd55af0adb9c99d
xuyisen <xuyisen123456@outlook.com>
2026-01-03 17:38:17 +0800

    update complex_expr in profile_me.rs; add README.md for profiling instructions

---
+++ README.md
@@ -0,0 +1,7 @@
+# OXIDICE
+
+## how to profile this application
+
+```
+cargo flamegraph --bin profile_me --release
+```
--- src/bin/profile_me.rs
+++ src/bin/profile_me.rs
@@ -1,11 +1,10 @@
 // src/bin/profile_me.rs
 use oxidice::parse_dice_and_show;
-use std::hint::black_box; // 替换为你的 crate 名字
+use std::hint::black_box;
 
 fn main() {
     // 1. 准备一个稍微复杂点的表达式
-    let complex_expr =
-        "(10d6!kh3dh3 + 5d20r<2r>3) * max(floor(3.5), 2) + filter>3([1,2,3]) + max(tolist(1d10))";
+    let complex_expr = "(10d6!kh3dh3 + 5d20r<2r>3) * max(floor(3.5), 2 + 2 + 4, min([1,2,3] + [1,2,3])) + filter>3([1,2,3]) + max(tolist(1d10))";
 
     // 2. 预热 (Warm up) - 触发 lazy_static 初始化
     let _ = parse_dice_and_show("1");
